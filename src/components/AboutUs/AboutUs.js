0553d4653ae88cc1d1bcd8e6e701538c40642b6f
Abdullah Awan <abdullahawan7676@gmail.com>
2022-09-24 23:29:45 +0500

    Responsive AboutUs Component for 300px to 640px: Carousel Added

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AboutUs.css';
+import CardsList from '../Card/CardsList';
 function AboutUs() {
     return (
         <div className='about-us' id='abt-us'>
@@ -12,115 +13,8 @@ function AboutUs() {
                     initiative of building a decentralized community by offering a diverse set of services in
                     its ever-evolving ecosystem.</span>
             </div>
-            <div className='cards-area'>
-                {/* Card: 1 */}
-                <div className='card'>
-                    <div className='card-heading'>
-                        <div>
-                            <img className='card-img' src="/img/simple-launch-pad-icon.png" alt="launch-pad_icon" />
-                        </div>
-                        <div className='card-name'>
-                            <span>Simple</span><span>Launchpad</span>
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        A simple Launchpad solution integrating
-                        with the IDO feature shall be offered to the users.This feature will create multiple
-                        opportunities for the users to participate and whitelist in an IDO project of their
-                        choice.
-                    </div>
-                </div>
-
-                {/* Card: 2 */}
-                <div className='card'>
-                    <div className='card-heading'>
-                        <div>
-                            <img className='card-img' src="/img/nft-launch-pad-icon.png" alt="launch-pad_icon" />
-                        </div>
-                        <div className='card-name'>
-                            <span>NFT</span><span>Launchpad</span>
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        NFT collectible projects аrе developing
-                        at a lightning speed and mоѕt of these projects adopt a ѕimilаr tоkеn ѕаlе
-                        structure.With Initiаl NFT Offеringѕ, Pacman Frog will diѕruрt thе NFT ecosystem with a
-                        full ѕtасk NFT incubator аnd a NFT lаunсhраd.
-                    </div>
-                </div>
-
-                {/* Card: 3 */}
-                <div className='card'>
-                    <div className='card-heading'>
-                        <div>
-                            <img className='card-img' src="/img/game-incubator-icon.png" alt="launch-pad_icon" />
-                        </div>
-                        <div className='card-name'>
-                            <span>Game</span><span>Incubator</span>
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        Pacman Frog incubates game
-                        еntrерrеnеurѕ аnd thеіr tеаmѕ, providing thеm with thе skills needed to run a blockchain
-                        gаmе ѕtudiо, рrоmоtе, and launch their firѕt рrоduсtѕ оn a glоbаl market.
-                    </div>
-                </div>
-
-                {/* Card: 4 */}
-                <div className='card'>
-                    <div className='card-heading'>
-                        <div>
-                            <img className='card-img' src="/img/nft-aggregator-icon.png" alt="launch-pad_icon" />
-                        </div>
-                        <div className='card-name'>
-                            <span>NFT</span><span>Aggregator</span>
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        Our NFT aggregator соllесtѕ аnd
-                        viѕuаlizеѕ rеаl-tіmе dаtа аrоund NFT аѕѕеtѕ and their trading volume асrоѕѕ different
-                        blосkсhаinѕ to provide uѕеrѕ with соmрrеhеnѕivе undеrѕtаnding оf NFT market trеndѕ.
-                    </div>
-                </div>
-
-                {/* Card: 5 */}
-                <div className='card'>
-                    <div className='card-heading'>
-                        <div>
-                            <img className='card-img' src="/img/gamify-nft-icon.png" alt="launch-pad_icon" />
-                        </div>
-                        <div className='card-name'>
-                            <span>Gam NFTeFi</span><span>Marketplace</span>
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        Pacman Frog is the оnlу marketplace
-                        that will allow the gamers to discover, purchase, and sell GameFi NFTs while also
-                        facilitating the primary market as well as the secondary market for the NFTs within the
-                        games.
-                    </div>
-                </div>
-
-                {/* Card: 6 */}
-                <div className='card'>
-                    <div className='card-heading'>
-                        <div>
-                            <img className='card-img' src="/img/on-board-icon.png" alt="launch-pad_icon" />
-                        </div>
-                        <div className='card-name'>
-                            <span>Onboard Users</span><span>to Crypto</span>
-                        </div>
-                    </div>
-                    <div className='card-content'>
-                        We want to make the users aware of
-                        everything latest and important happening in the diverse world of Blockchain including
-                        Cryptocurrency, Solana, Decentralized Finance, NFTs, GameFi, Metaverse, Consensus
-                        Mechanism, and much more.
-                    </div>
-                </div>
-
-            </div>
-        </div>
+            <CardsList />
+        </div >
     )
 }
 
