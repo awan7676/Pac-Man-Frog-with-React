44b8345c204b05330769eca7b7b74237d2f1f5c9
Abdullah Awan <abdullahawan7676@gmail.com>
2022-09-26 19:43:59 +0500

    HowDoWeDo Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./components/Home/Home";
 import AboutUs from "./components/AboutUs/AboutUs";
+import HowDoWeDo from "./components/HowDoWeDo/HowDoWeDo";
 function App() {
   return (
     <div className="App">
       <Home />
       <AboutUs />
+      <HowDoWeDo />
     </div>
   );
 }
