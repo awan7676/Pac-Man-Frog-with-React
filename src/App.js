5a9a76cb7ab08cc9a792770a94e28517f340226b
Abdullah Awan <abdullahawan7676@gmail.com>
2022-09-27 23:02:29 +0500

    RoadMap Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./components/Home/Home";
 import AboutUs from "./components/AboutUs/AboutUs";
 import HowDoWeDo from "./components/HowDoWeDo/HowDoWeDo";
+import RoadMap from "./components/RoadMap/RoadMap";
 function App() {
   return (
     <div className="App">
       <Home />
       <AboutUs />
       <HowDoWeDo />
+      <RoadMap />
     </div>
   );
 }
