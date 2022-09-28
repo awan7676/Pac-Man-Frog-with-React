092f8814e27d100871c087b202f6d6f944beed74
Abdullah Awan <abdullahawan7676@gmail.com>
2022-09-29 00:27:45 +0500

    PowerOfPac Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home/Home";
 import AboutUs from "./components/AboutUs/AboutUs";
 import HowDoWeDo from "./components/HowDoWeDo/HowDoWeDo";
 import RoadMap from "./components/RoadMap/RoadMap";
+import PowerOfPac from "./components/PowerOfPac/PowerOfPac";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <AboutUs />
       <HowDoWeDo />
       <RoadMap />
+      <PowerOfPac />
     </div>
   );
 }
